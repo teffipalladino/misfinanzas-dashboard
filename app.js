22ef8f74c3b487270dd3cab62dba5c1249ee3372
teffipalladino <teffi.palladino@gmail.com>
2025-06-10 17:58:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ import { Sidebar } from './components/Sidebar.js';
 document.addEventListener('DOMContentLoaded', () => {
   Sidebar();
 
+  const mainContainer = document.getElementById('main');
+
+  function limpiarMain() {
+    mainContainer.innerHTML = '';
+  }
+
   document.getElementById('precioVentaBtn').addEventListener('click', () => {
+    limpiarMain();
     import('./components/CalculadoraPrecio.js')
-      .then(mod => mod.renderCalculadoraPrecioVenta());
+      .then(mod => mod.renderCalculadoraPrecioVenta(mainContainer));
   });
 
   document.getElementById('puntoEquilibrioBtn').addEventListener('click', () => {
+    limpiarMain();
     import('./components/CalculadoraPuntoEquilibrio.js')
-      .then(mod => mod.PuntoEquilibrio());
+      .then(mod => mod.PuntoEquilibrio(mainContainer));
   });
 
-  // Carga inicial: calculadora de precio de venta
+  // Carga inicial: Precio de venta
+  limpiarMain();
   import('./components/CalculadoraPrecio.js')
-    .then(mod => mod.renderCalculadoraPrecioVenta());
+    .then(mod => mod.renderCalculadoraPrecioVenta(mainContainer));
 });
